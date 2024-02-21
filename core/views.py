3ab1f181f583559d0db75f71a8baa81e7aeefbf8
arvindrk47 <arvindrk47@gmail.com>
2024-02-21 18:57:20 +0530

    Added base level views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .forms import PricingConfigForm, PricingTierForm
+from .models import PricingConfig, PricingTier
+from rest_framework import status
 # Create your views here.
+
+def pricing_config(request):
+    if request.method == 'POST':
+        if 'config_id' in request.POST:
+            config = PricingConfig.objects.get(id=request.POST['config_id'])
+            form = PricingConfigForm(request.POST, instance = config)
+        else:
+            form = PricingConfigForm(request.POST)
+        
+
+        if form.is_valid():
+            config = form.save(commit = False)
+            config.created_by = request.user
+            config.save()
+            return status(status = status.HTTP_201_CREATED)
+        
