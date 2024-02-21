f0a7fb33278a35fc5c512219ea357c5cdd48220e
arvindrk47 <arvindrk47@gmail.com>
2024-02-21 20:08:50 +0530

    Added views.py and urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('config/', views.pricing_config, name='pricing_config'),
+    path('tier/<int:config_id>/', views.pricing_tier, name='pricing_tier'),
+    path('tier/add/<int:config_id>/', views.pricing_tier, name='pricing_tier_add'),
+    path('tier/edit/<int:tier_id>/', views.pricing_tier, name='pricing_tier_edit'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,20 +2,36 @@ from django.shortcuts import render
 from .forms import PricingConfigForm, PricingTierForm
 from .models import PricingConfig, PricingTier
 from rest_framework import status
-# Create your views here.
 
 def pricing_config(request):
     if request.method == 'POST':
         if 'config_id' in request.POST:
             config = PricingConfig.objects.get(id=request.POST['config_id'])
-            form = PricingConfigForm(request.POST, instance = config)
+            form = PricingConfigForm(request.POST, instance=config)
         else:
             form = PricingConfigForm(request.POST)
-        
-
         if form.is_valid():
-            config = form.save(commit = False)
+            config = form.save(commit=False)
             config.created_by = request.user
             config.save()
-            return status(status = status.HTTP_201_CREATED)
-        
+            return render(request, 'pricing/config_list.html', {'configs': PricingConfig.objects.all()})
+    else:
+        form = PricingConfigForm()
+    return render(request, 'pricing/config_form.html', {'form': form})
+
+def pricing_tier(request, config_id):
+    config = PricingConfig.objects.get(id=config_id)
+    if request.method == 'POST':
+        if 'tier_id' in request.POST:
+            tier = PricingTier.objects.get(id=request.POST['tier_id'])
+            form = PricingTierForm(request.POST, instance=tier)
+        else:
+            form = PricingTierForm(request.POST)
+        if form.is_valid():
+            tier = form.save(commit=False)
+            tier.config = config
+            tier.save()
+            return render(request, 'pricing/tier_list.html', {'config': config, 'tiers': PricingTier.objects.filter(config=config)})
+    else:
+        form = PricingTierForm()
+    return render(request, 'pricing/tier_form.html', {'form': form, 'config': config})
